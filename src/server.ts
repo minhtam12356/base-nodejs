e5682ab06a59592fda35a18972198f5e0c0f3313
Minh Tâm <tam26918@gmail.com>
2022-05-15 23:30:32 +0700

    add connect mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
+import cors from 'cors';
 import 'dotenv/config';
 import express from "express";
+import mongoose from 'mongoose';
 
 const app = express();
+const { PORT, MONGODB_URI = "" } = process.env;
 
-const port = process.env.PORT || 3001;
+const port = PORT || 3001;
+
+app.use(cors());
+//===STATIC FILE===
+app.use(express.static('public'));
+
+// connect mongoDB
+mongoose.connect(MONGODB_URI, function (err) {
+    if (err) 
+        throw err;
+    console.log('Successfully connected');
+})
 
 //===LISTEN PORT===
 app.listen(port, function(){
